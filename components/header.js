b9532944e20c8a6cb5a250fdefec301077817809
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-09-11 11:51:36 +0200

    I created a header component. with only the logo on it.

---
+++ components/header.js
@@ -0,0 +1,21 @@
+// components/Header.js
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">
+          <Link href="/">
+            MyLogo
+          </Link>
+        </div>
+
+        <div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
