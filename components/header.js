90ee590898884606fc57326a3ddea04e97ad69e2
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-09-12 14:56:25 +0200

    changed the styling of my header. the name and color.

--- components/header.js
+++ components/header.js
@@ -1,17 +1,15 @@
-
 // components/Header.js
 import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-sky-500 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">
           <Link href="/">
-            MyLogo
+            SHOPQUICK
           </Link>
         </div>
-
       </div>
     </header>
   );
