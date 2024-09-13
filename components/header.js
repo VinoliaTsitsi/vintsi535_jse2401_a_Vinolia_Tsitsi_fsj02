aa2ad5bdcd098b809a1b8e8729ff4259699bbc41
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-09-13 08:32:04 +0200

    Changed the styling on the header, made the blue lighter.

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="bg-sky-500 text-white p-4">
+    <header className="bg-sky-400 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">
           <Link href="/">
