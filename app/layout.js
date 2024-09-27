65ea09a817d9977b50e2f42bb0141a22021eeafe
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-09-27 16:40:41 +0200

    Remove Header component import and usage from RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
 import "./globals.css";
-import Header from './components/header'; // Import the Header component
 
 /**
  * RootLayout component provides a consistent layout structure for the application.
  *
- * This component includes the Header component at the top and renders the children components
- * passed to it. It also sets up global HTML and body attributes, such as language and font styling.
+ * This component sets up global HTML and body attributes, such as language and font styling.
  * 
  * @param {Object} props - Component properties.
  * @param {React.ReactNode} props.children - The content to be rendered within the layout.
@@ -23,9 +21,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased">
-        {/* Add the Header component here */}
-        <Header />
-        
         {/* Render the children */}
         {children}
       </body>
