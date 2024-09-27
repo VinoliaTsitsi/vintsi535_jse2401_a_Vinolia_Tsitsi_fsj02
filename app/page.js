1ce146357cef02645e813349f27b4c88db507f75
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-09-27 16:39:55 +0200

    Remove RootLayout component from HomePage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import RootLayout from './layout'; // Import the RootLayout
 import Products from './components/products';
 import { fetchProducts } from '../lib/api';
 import './globals.css';
@@ -24,20 +23,16 @@ export default async function HomePage() {
   try {
     const initialProducts = await fetchProducts(1);
     return (
-      <RootLayout>
-        <main>
-          <Products initialProducts={initialProducts} />
-        </main>
-      </RootLayout>
+      <main>
+        <Products initialProducts={initialProducts} />
+      </main>
     );
   } catch (error) {
     return (
-      <RootLayout>
-        <main>
-          <h1>Error fetching products</h1>
-          <p>There was an error fetching the product data. Please try again later.</p>
-        </main>
-      </RootLayout>
+      <main>
+        <h1>Error fetching products</h1>
+        <p>There was an error fetching the product data. Please try again later.</p>
+      </main>
     );
   }
 }
